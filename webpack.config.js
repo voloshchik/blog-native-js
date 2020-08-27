1a2b8f68f200f607fac475673bf208b91359463e
vladimir <lovkiy2012@gmail.com>
2020-08-27 15:55:21 +0300

    seting babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ["./src/index.js"],
-  output: {
-    path: __dirname + "/dist",
-    filename: "bundle.js",
-  },
-  devServer: {
-    contentBase: __dirname + "/dist",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/index.html",
-    }),
-  ],
-  resolve: {
-    extensions: [".js"],
-  },
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: __dirname + '/dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+        }),
+    ],
+    resolve: {
+        extensions: ['.js'],
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+        ],
+    },
 };
