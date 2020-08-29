326bb6a2fc3ed52817f93fb18d2b77fd046f4ba0
vladimir <lovkiy2012@gmail.com>
2020-08-29 14:44:02 +0300

    11 form validation

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,5 +1,6 @@
 import { Component } from '../core/component';
 import { Form } from '../core/form';
+import { Validators } from '../core/validators';
 
 export class CreateComponent extends Component {
     constructor(id) {
@@ -10,18 +11,23 @@ export class CreateComponent extends Component {
     init() {
         this.$el.addEventListener('submit', submitHandler.bind(this));
         this.form = new Form(this.$el, {
-            title: [],
-            fulltext: [],
+            title: [Validators.required],
+            fulltext: [Validators.required, Validators.minLength(5)],
         });
     }
 }
 
 function submitHandler(e) {
     e.preventDefault();
-    const formData = {
-        type: this.$el.type.value,
-        ...this.form.value(),
-    };
 
-    console.log(formData);
+    if (this.form.isValid()) {
+        const formData = {
+            type: this.$el.type.value,
+            ...this.form.value(),
+        };
+
+        console.log(formData);
+    } else {
+        console.warn('Fomrm is valid');
+    }
 }
